67d323481f764f8939d695e83fa8538f911a8fdc
Carlos Mendoza <carlos_mendoza2@hotmail.com>
2023-08-09 14:45:09 -0400

    added new poem in script.js file. added a new photo in assets for this poem. changed background color

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },
+  {
+    author: "Robert Frost",
+    poem: "Some say the world will end in fire. \n Some say in ice. \n From what i've tasted of desire I hold with those who favor fire. \n But if it had to perish twice, \n I think I know enough of hate \n To say that for destruction ice \n is also great \n And would suffice",
+    image: "fireice.jpg",
+  },
 
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
 ];
